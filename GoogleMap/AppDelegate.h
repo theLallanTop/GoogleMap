f8925e10df4989a3faabdb6917c6f37f5a551d10
Manjeet <manjeet@Manjeets-Mac-mini.local>
2017-05-04 01:24:13 +0530

    google map

---
+++ GoogleMap/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  GoogleMap
+//
+//  Created by Manjeet on 04/05/17.
+//  Copyright © 2017 Manjeet. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ GoogleMap/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  GoogleMap
+//
+//  Created by Manjeet on 04/05/17.
+//  Copyright © 2017 Manjeet. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
